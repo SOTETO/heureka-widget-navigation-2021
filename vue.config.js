9267e624fecb3a5a1bccc19ab6d1939cb34f1aed
Johann Sell <sell@informatik.hu-berlin.de>
2021-12-10 14:15:31 +0100

    close #6 and increase version numbers of dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/'
-        : '/new/',
+        : '/heureka-widget-navigation/',
     chainWebpack: config => {
         const svgRule = config.module.rule('svg')
 
